fd78591791d44230fb0e3f497f402488b32318b2
Мясоедов Захар <zahar_myasoedov@mail.ru>
2021-10-02 12:25:15 +0700

    Добавьте файлы проекта.

---
+++ WpfApp2/MainWindow.xaml.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfApp2
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+
+        ZMYA2Entities context;
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            context = new ZMYA2Entities();
+            ShowTable();
+
+            
+            
+        }
+
+        
+        private void ShowTable()
+        {
+            DataGrid.ItemsSource = context.albums.ToList();
+        }
+        private void ButtonAdd_Click(object sender, RoutedEventArgs e)
+        {
+            var newRegistration = new groups();
+            var newreg = new songs();
+            context.groups.Add(newRegistration);
+            context.songs.Add(newreg);
+
+            var win = new Window1(context, newRegistration, newreg);
+            win.ShowDialog();
+        }
+
+        private void ButtonDelete_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void ButtonEdit_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Form2_Click(object sender, RoutedEventArgs e)
+        {
+            //Window1 window1 = new Window1();
+            //window1.Show();
+        }
+    }
+}
---
+++ WpfApp2/Window1.xaml.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace WpfApp2
+{
+    /// <summary>
+    /// Логика взаимодействия для Window1.xaml
+    /// </summary>
+    public partial class Window1 : Window
+    {
+        ZMYA2Entities context;
+        
+        public Window1(ZMYA2Entities context, groups groups, songs songs)
+        {
+            InitializeComponent();
+
+            this.context = context;
+            //CmbGroup.Text = context.groups.ToString();
+            //CmbSongs.Text = context.songs.ToString();
+            this.DataContext = groups;
+            //this.DataContext = songs;
+        }
+
+        private void CmbArtists_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+
+        }
+
+        private void BtnSave_Click(object sender, RoutedEventArgs e)
+        {
+
+            string dksks = CmbGroup.Text;
+            var reg = (groups)this.DataContext;
+            reg.group_name = dksks;
+            context.SaveChanges();
+            //string mgklsj = CmbSongs.Text;
+            //var reg1 = (songs)DataContext;
+            //reg1.song_name = mgklsj;
+            //context.SaveChanges();
+        }
+
+        private void CmbGroup_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+
+        }
+
+        //private void BtnSave2_Click(object sender, RoutedEventArgs e)
+        //{
+        //    string dksks = CmbGroup.Text;
+        //    var reg = (groups)this.DataContext;
+        //    reg.group_name = dksks;
+        //    context.SaveChanges();
+        //}
+    }
+}
